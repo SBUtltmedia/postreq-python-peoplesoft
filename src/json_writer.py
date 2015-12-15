2cdf9001e0c50302e89cb3f58bb19606a2e8895b
TLT Media Lab <tltmedia@gmail.com>
2015-12-15 16:54:55 -0500

    Refactored json_writer

--- src/json_writer.py
+++ src/json_writer.py
@@ -1,11 +1,21 @@
 import json
+import os
+
+
+def create_dir(path):
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+
+
+def write(data, file, indent=None):
+    create_dir(file)
+    with open(file, 'w+') as outfile:
+        json.dump(data, outfile, indent=indent)
 
 
 def write_pretty(data, file):
-    with open(file, 'w') as outfile:
-        json.dump(data, outfile, indent=2)
+    write(data, file, indent=2)
 
 
 def write_minified(data, file):
-    with open(file, 'w') as outfile:
-        json.dump(data, outfile)
+    write(data, file)
