4b88a76b9f9cfc605a69c3013be33a11feb06140
TLT Media Lab <tltmedia@gmail.com>
2015-12-15 16:37:25 -0500

    Sorting output by course name

--- src/main.py
+++ src/main.py
@@ -4,26 +4,26 @@ import json
 #import cProfile
 
 
-def get_requirement(requirements, course):
+def get_course_reqs(requirements, course):
     if course.rq_group is not None:
         if course.rq_group in requirements:
             return requirements[course.rq_group]
     return ""
 
 
-def match_requirements_to_course(requirements, course):
+def match_reqs_to_course(requirements, course):
     return {
         "course_id": course.course_id,
         "course": str(course),
-        "requirements": get_requirement(requirements, course)
+        "requirements": get_course_reqs(requirements, course)
     }
 
 
-def match_requirements_to_courses(requirements, courses):
-    return tuple(map(lambda c: match_requirements_to_course(requirements, c), courses))
+def match_reqs_to_courses(requirements, courses):
+    return tuple(map(lambda c: match_reqs_to_course(requirements, c), courses))
 
 
-def replace_course_ids_with_catalog_numbers(requirements, courses):
+def replace_course_ids_with_names(requirements, courses):
     def f(r):
         for course in courses:
             if course.course_id in r:
@@ -32,15 +32,18 @@ def replace_course_ids_with_catalog_numbers(requirements, courses):
     return {group: f(reqs) for group, reqs in requirements.items()}
 
 
+def write(data):
+    with open('output/requirements.json', 'w') as outfile:
+        json.dump(data, outfile, indent=2)
+
+
 def main():
     courses = read_courses()
-    requirements = read_requirements()
-    sifted_requirements = sift(requirements)
-    readable_requirements = replace_course_ids_with_catalog_numbers(sifted_requirements, courses)
-    courses_with_requirements = match_requirements_to_courses(readable_requirements, courses)
-
-    with open('output/requirements.json', 'w') as outfile:
-        json.dump(courses_with_requirements, outfile, indent=2)
+    reqs = read_requirements()
+    sifted_reqs = sift(reqs)
+    readable_reqs = replace_course_ids_with_names(sifted_reqs, courses)
+    courses_with_reqs = match_reqs_to_courses(readable_reqs, courses)
+    write(sorted(courses_with_reqs, key=lambda c: c['course']))
 
 #cProfile.run('main()')
 main()
